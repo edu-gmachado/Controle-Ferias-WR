96e37241136b48bc857799236b4d39692c980f53
edu-gmachado <edgeraldo12@gmail.com>
2026-07-09 18:40:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'controle-ferias-3turno-v6.0.3.0';
+const CACHE_NAME = 'controle-ferias-3turno-v6.4.0.0';
 const APP_SHELL = [
   './',
   './index.html',
-  './styles.css?v=6.0.3',
-  './app.js?v=6.0.3',
-  './firebase-config.js?v=6.0.3',
-  './auth-service.js?v=6.0.3',
-  './firebase-service.js?v=6.0.3',
+  './styles.css?v=6.4',
+  './app.js?v=6.4',
+  './firebase-config.js?v=6.4',
+  './auth-service.js?v=6.4',
+  './firebase-service.js?v=6.4',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
